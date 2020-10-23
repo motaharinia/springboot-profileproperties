c89c3b49c5f49ac83e8cbf56025be2459a46c1a2
MostafaMotaharinia <eng.motahari@gmail.com>
2020-10-24 00:53:17 +0330

    update test annotations

--- src/test/java/com/motaharinia/profileproperties/ProfilePropertiesApplicationTests.java
+++ src/test/java/com/motaharinia/profileproperties/ProfilePropertiesApplicationTests.java
@@ -1,9 +1,16 @@
 package com.motaharinia.profileproperties;
 
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("dev")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ProfilePropertiesApplicationTests {
 
     @Test
